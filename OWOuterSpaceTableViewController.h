04948615ea1f40ce64ceefd4ed685ffdc4daa857
Avishaan <avishaan@codeHatcher.com>
2014-09-08 11:54:39 -0700

    basic table view working

---
+++ OWOuterSpaceTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  OWOuterSpaceTableViewController.h
+//  OutofThisWorld
+//
+//  Created by Brown Magic on 9/5/14.
+//  Copyright (c) 2014 codeHatcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWOuterSpaceTableViewController : UITableViewController
+@property (strong, nonatomic) NSMutableArray *planets;
+
+@end
---
+++ OutofThisWorld/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  OutofThisWorld
+//
+//  Created by Brown Magic on 9/5/14.
+//  Copyright (c) 2014 codeHatcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
