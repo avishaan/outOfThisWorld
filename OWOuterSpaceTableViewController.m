b352d34535c1fe536e142f526b30aa29c2e974a1
Avishaan <avishaan@codeHatcher.com>
2014-09-09 21:46:59 -0700

    updated view controller

--- OWOuterSpaceTableViewController.m
+++ OWOuterSpaceTableViewController.m
@@ -32,7 +32,6 @@
   // bar for this view controller.
   // self.navigationItem.rightBarButtonItem = self.editButtonItem;
   
-  self.planets = [NSMutableArray new];
   NSString *planet1 = @"Mercury";
   NSString *planet2 = @"Venus";
   NSString *planet3 = @"Earth";
@@ -42,14 +41,8 @@
   NSString *planet7 = @"Uranus";
   NSString *planet8 = @"Neptune";
   
-  [self.planets addObject:planet1];
-  [self.planets addObject:planet2];
-  [self.planets addObject:planet3];
-  [self.planets addObject:planet4];
-  [self.planets addObject:planet5];
-  [self.planets addObject:planet6];
-  [self.planets addObject:planet7];
-  [self.planets addObject:planet8];
+  self.planets = [[NSMutableArray alloc] initWithObjects: planet1, planet2, planet3,
+                  planet4, planet5, planet6, planet7, planet8];
   
 //  NSMutableDictionary *myDictionary = [NSMutableDictionary new];
 //  NSString *firstColor = @"red";
--- OutofThisWorld/OWSpaceObject.h
+++ OutofThisWorld/OWSpaceObject.h
@@ -10,4 +10,19 @@
 
 @interface OWSpaceObject : NSObject
 
+@property (strong, nonatomic) NSString *name;
+@property (nonatomic) float gravitationalForce;
+@property (nonatomic) float diameter;
+@property (nonatomic) float yearLength;
+@property (nonatomic) float dayLength;
+@property (nonatomic) float temperature;
+@property (nonatomic) int numberOfMoons;
+@property (strong, nonatomic) NSString *nickname;
+@property (nonatomic, strong) NSString *interestFact;
+
+@property (nonatomic, strong) UIImage *spaceImage;
+
+-(id)initWithData:(NSDictionary *)data andImage:(UIImage *)image;
+
+
 @end
